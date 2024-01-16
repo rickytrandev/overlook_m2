fe1f2ba10a780a0c62d0ed0eb31b28c2e52026e0
Ricky Tran <therichardtran@gmail.com>
2024-01-15 17:49:37 -0700

    feat: user can filter rooms by room type, additionally, they can reserve the room

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -13,3 +13,48 @@ export function getData(url) {
 			console.log(error);
 		})
 }
+
+export function bookRoom(roomNumber, fromDate, userID) {
+  fetch('http://localhost:3001/api/v1/bookings', {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ userID: userID, date: fromDate, roomNumber: roomNumber })
+  })
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        console.log("status", res.status);
+        return res.json().then(errorData => {
+          throw new Error(errorData.message || 'Unable to reserve the room. Please give us a call');
+        });
+      }
+    })
+    .then(data => console.log(data))
+    .catch(error => {
+      console.log(error.message);
+    });
+}
+
+export function postRecipe() {
+  fetch('http://localhost:3001/api/v1/usersRecipes', {
+    method: 'POST',
+    headers: { 'Content-Type' : 'application/json' },
+    body: JSON.stringify({ userID: currentUser.id, recipeID: currentRecipe.id })
+  })
+    .then(response => {
+      if (response.ok) {
+        addFavoriteRecipe(currentUser, currentRecipe)
+        return response.json()
+      } else {
+        console.log("status", response.status);
+        throw new Error('Unable to save recipe')
+      }
+    })
+    .then(data => console.log(data))
+    .catch(error => {
+      console.log(error);
+      closeModal()
+      recipeCardSection.innerHTML = `${error.message}`
+    })
+}
--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import { getData } from './apiCalls';
+import { getData, bookRoom } from './apiCalls';
 import { findUserCurrentBookings, findRoomDetails, getTotalSpent, filterRooms, getAvailableRooms } from './users';
 
 // selectors
@@ -25,6 +25,7 @@ let currentUser;
 let allBookings;
 let allRooms;
 let availableRooms;
+let fromDate
 let filteredRoomTypes = []
 
 // event listeners
@@ -37,16 +38,21 @@ $(window).on('load', () => {
 		.then(data => allRooms = data.rooms);
 });
 
+
+
+
 loginForm.submit((e) => {
 	e.preventDefault()
-	const username = $('.input-field[type="text"]').val();
-  const password = $('.input-field[type="password"]').val();
+	// const username = $('.input-field[type="text"]').val();
+  // const password = $('.input-field[type="password"]').val();
+	let username = 'customer50'
+	let password = 'overlook'
 	userID = Number(username.match(/\d+/));
-
+	
 	if(username && password) {
 		let findUser = users.find(user => user.id === userID)
-	
 		if (username ==`customer${userID}` && findUser && password === 'overlook') {
+		// if (username ==`customer${userID}` && findUser && password === 'overlook') {
 			currentUser = findUser
 			console.log('User found');
 			loginError.addClass('hidden')
@@ -70,32 +76,21 @@ loginForm.submit((e) => {
 
 searchRoomsForm.submit((e) => {
 	e.preventDefault()
-	let fromDate = $('#from-date').val().replaceAll('-','/')
+	fromDate = $('#from-date').val().replaceAll('-','/')
+	// TODO: delete hard coded from date below
+	fromDate = '2024/01/22'
 	let toDate = $('#to-date').val()
 	
 	if(fromDate) {
 		$('.filters').removeClass('hidden')
 		availableRooms = getAvailableRooms(allBookings, fromDate, allRooms)
 		if (availableRooms) {
-			availableRoomsSection.html('<h2 class="booking-title" >Available Rooms</h2>')
-			console.log("available rooms", availableRooms);
 			$('available-rooms-error').removeClass('hidden')
 			availableRoomsText.addClass('hidden')
-			availableRooms.forEach(room => {
-				availableRoomsSection.html(availableRoomsSection.html() + `
-				<div class="bookings-card">
-				<p class="descriptor"> Room Type: <span>${room.roomType}</span></p>
-				<p class="descriptor"> Bed Size: <span>${room.bedSize}</span></p>
-				<p class="descriptor"> Beds: <span>${room.numBeds}</span></p>
-				<p class="descriptor"> Per Night: <span>$${room.costPerNight}</span></p>
-				<button>View</button> 
-				</div>
-				`);
-			})
+			generateAvailableRooms(availableRooms)
 		} else {
 			$('available-rooms-error').removeClass('hidden')
 				}
-
 	} else {
 		calendarError.removeClass('hidden')
 		bookStayText.addClass('hidden')
@@ -118,18 +113,7 @@ checkboxes.each(function() {
 				<p class="filter-rooms-error">We are sorry, there are no rooms that match your criteria. Please adjust your filters.</p>
 				`)
 			} else {
-				availableRoomsSection.html('<h2 class="booking-title" >Available Rooms</h2>')
-				filteredRooms.forEach(room => {
-					availableRoomsSection.html(availableRoomsSection.html() + `
-					<div class="bookings-card">
-					<p class="descriptor"> Room Type: <span>${room.roomType}</span></p>
-					<p class="descriptor"> Bed Size: <span>${room.bedSize}</span></p>
-					<p class="descriptor"> Beds: <span>${room.numBeds}</span></p>
-					<p class="descriptor"> Per Night: <span>$${room.costPerNight}</span></p>
-					<button>View</button> 
-					</div>
-					`);
-				})
+				generateAvailableRooms(filteredRooms)
 			}
 			console.log('available rooms',availableRooms)
 			console.log('filtered room types', filteredRoomTypes)
@@ -140,44 +124,44 @@ checkboxes.each(function() {
 			let filteredRooms = filterRooms(availableRooms, filteredRoomTypes)
 
 			if (allUnchecked) {
-				availableRoomsSection.html('<h2 class="booking-title" >Available Rooms</h2>')
-				availableRooms.forEach(room => {
-					availableRoomsSection.html(availableRoomsSection.html() + `
-					<div class="bookings-card">
-					<p class="descriptor"> Room Type: <span>${room.roomType}</span></p>
-					<p class="descriptor"> Bed Size: <span>${room.bedSize}</span></p>
-					<p class="descriptor"> Beds: <span>${room.numBeds}</span></p>
-					<p class="descriptor"> Per Night: <span>$${room.costPerNight}</span></p>
-					<button>View</button> 
-					</div>
-					`);
-				})
+				generateAvailableRooms(availableRooms)
 			} else {
 					if(filteredRooms.length === 0) {
 						availableRoomsSection.html(`
 						<p class="filter-rooms-error">We are sorry, there are no rooms that match your criteria. Please adjust your filters.</p>
 						`)
 					} else {
-						availableRoomsSection.html('<h2 class="booking-title" >Available Rooms</h2>')
-						filteredRooms.forEach(room => {
-							availableRoomsSection.html(availableRoomsSection.html() + `
-							<div class="bookings-card">
-							<p class="descriptor"> Room Type: <span>${room.roomType}</span></p>
-							<p class="descriptor"> Bed Size: <span>${room.bedSize}</span></p>
-							<p class="descriptor"> Beds: <span>${room.numBeds}</span></p>
-							<p class="descriptor"> Per Night: <span>$${room.costPerNight}</span></p>
-							<button>View</button> 
-							</div>
-							`);
-						})
+						generateAvailableRooms(filteredRooms)
+						}
 				}
-			}
 		}
 	})
 }) 
 
 
 // functions
+function generateAvailableRooms(rooms) {
+	availableRoomsSection.html('<h2 class="booking-title" >Available Rooms</h2>')
+	rooms.forEach(room => {
+		availableRoomsSection.html(availableRoomsSection.html() + `
+		<div class="bookings-card">
+		<p class="descriptor"> Room Type: <span>${room.roomType}</span></p>
+		<p class="descriptor"> Bed Size: <span>${room.bedSize}</span></p>
+		<p class="descriptor"> Beds: <span>${room.numBeds}</span></p>
+		<p class="descriptor"> Per Night: <span>$${room.costPerNight}</span></p>
+		<button id="${room.number}" class="reserve-btn">Reserve</button>
+		</div>
+		`)
+		$('.reserve-btn').click(function(e) {
+			const roomNumber = Number(e.target.id)
+			bookRoom(roomNumber, fromDate, userID)
+			getData('http://localhost:3001/api/v1/bookings')
+			.then(data => allBookings = data.bookings);
+			$(`#${String(roomNumber)}`).parent().remove()
+		});
+	})
+}
+
 function generateUserBookedRooms() {
 	const usersBookedRooms = findUserCurrentBookings(currentUser, allBookings)
 	const roomDetails = findRoomDetails(usersBookedRooms, allRooms)
@@ -192,7 +176,6 @@ function generateUserBookedRooms() {
 		<p class="descriptor"> Bed Size: <span>${room.bedSize}</span></p>
 		<p class="descriptor"> Beds: <span>${room.numBeds}</span></p>
 		<p class="descriptor"> Per Night: <span>$${room.costPerNight}</span></p>
-		<button>View</button> 
 		</div>
 		`);
 	})
